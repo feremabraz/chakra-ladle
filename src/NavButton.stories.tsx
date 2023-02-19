4e29e78da78aa6d0c34185a8e4c577cce61af6ba
Fernando <fb@gamekaiju.monster>
2023-02-19 04:47:06 +0000

    NavButton component.

---
+++ src/NavButton.stories.tsx
@@ -0,0 +1,26 @@
+import { As, Button, ButtonProps, HStack, Icon, Text } from "@chakra-ui/react";
+import { HouseSimple } from "phosphor-react";
+
+interface NavButtonProps extends ButtonProps {
+  icon: As;
+  label: string;
+}
+
+export const NavButton = (props: NavButtonProps) => {
+  const { icon, label, ...buttonProps } = props;
+  return (
+    <Button variant="ghost" justifyContent="start" {...buttonProps}>
+      <HStack spacing="3">
+        <Icon as={icon} boxSize="6" color="subtle" />
+        <Text>{label}</Text>
+      </HStack>
+    </Button>
+  );
+};
+
+NavButton.args = {
+  label: "Home",
+  icon: () => {
+    return <HouseSimple size={20} />;
+  },
+};
